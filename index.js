812c994c6de3bee923310201590d65685a034a47
imcarissa <carissa.sarreal@gmail.com>
2021-05-17 20:28:40 -0400

    Done.

--- index.js
+++ index.js
@@ -8,20 +8,16 @@ function updateDriverWithKeyAndValue(driver, key, value) {
     return newObj;
 }
 
-
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
     driver[key] = value;
     return driver;
 }
-
-
 
 function deleteFromDriverByKey(driver, key) {
     const newObj = {...driver}
     delete newObj[key];
     return newObj;
 }
-
 
 function destructivelyDeleteFromDriverByKey(driver, key) {
     delete driver[key];
